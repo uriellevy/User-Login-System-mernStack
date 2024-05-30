3badda44b488d0afeb3c149bd7b67f5d8117e859
uriellevy <uriellevy4892@gmail.com>
2024-05-30 13:20:21 +0300

    morgan and chalk libraries added to node js

--- server/src/server.js
+++ server/src/server.js
@@ -4,12 +4,17 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import todoRoutes from './routes/todo.js';
 import userRoutes from './routes/user.js';
+import morgan from 'morgan';
+import chalk from 'chalk';
 
+const log = console.log;
+log(chalk.blue.bgRed.bold('Hello world!'));
 
 const PORT = process.env.PORT || 4005;
 
 dotenv.config();
 const app = express();
+app.use(morgan('dev'));
 
 app.use(cors());
 app.use(express.json());
